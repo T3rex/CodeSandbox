4350041e9774b90fb7146ca832d7d66a6f7335a7
T3rex <31337aditya@gmail.com>
2025-07-28 03:06:05 +0530

    Added sorting in tree structure

--- frontend/src/components/molecules/Tree/TreeNode.jsx
+++ frontend/src/components/molecules/Tree/TreeNode.jsx
@@ -1,9 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { SlArrowRight, SlArrowDown } from "react-icons/sl";
 import "./TreeNode.css";
 
 function TreeNode({ fileFolderData }) {
   const [visibility, setVisibility] = useState({});
+  const sortedChildren = useMemo(() => {
+    if (!fileFolderData.children) return [];
+    return [
+      ...fileFolderData.children.filter((c) => c.children),
+      ...fileFolderData.children.filter((c) => !c.children),
+    ];
+  }, [fileFolderData.children]);
 
   const toggleVisibility = (name) => {
     setVisibility((prev) => ({ ...prev, [name]: !prev[name] }));
@@ -29,7 +36,7 @@ function TreeNode({ fileFolderData }) {
 
       {visibility[fileFolderData.name] &&
         fileFolderData.children &&
-        fileFolderData.children.map((child) => {
+        sortedChildren.map((child) => {
           return (
             <div className="tree-children" key={child.name}>
               <TreeNode fileFolderData={child} />
